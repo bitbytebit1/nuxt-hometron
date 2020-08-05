0adb4f22e410fd794ed3a40bd686145b36e15c58
bitbytebit <tribe321@gmail.com>
2020-08-05 20:23:26 +0200

    api/index.js - add preview route

--- api/index.js
+++ api/index.js
@@ -6,12 +6,14 @@ const browse = require('./routes/browse')
 const download = require('./routes/download')
 const open = require('./routes/open')
 const autoit = require('./routes/autoit')
+const preview = require('./routes/preview')
 
 // register browse route
 app.use(browse)
 app.use(download)
 app.use(open)
 app.use(autoit)
+app.use(preview)
 
 app.get('/', function (req, res) {
   res.json({
@@ -37,6 +39,10 @@ app.get('/', function (req, res) {
       egUrl: 'http://localhost:3000/api/open?file=c:\\Some.txt',
       params: 'file - file to execute',
       desc: 'Lists available routes'
+    preview: {
+      egUrl: 'http://localhost:3000/api/preview?file=c:\\Some.txt',
+      params: 'file - a file to preview',
+      desc: 'Preview a file'
     }
   })
 })
