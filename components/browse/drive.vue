e20e4b5f0a469f4d85c0ac28cbf2e59c6e919929
bitbytebit <tribe321@gmail.com>
2021-05-30 01:10:16 +0200

    feat: drive size

--- components/browse/drive.vue
+++ components/browse/drive.vue
@@ -5,6 +5,9 @@
   >
     <v-list-item-content>
       <v-list-item-title v-text="item.name" />
+      <v-list-item-subtitle>
+        {{ formatBytes(item.spaceAvailable) }} of {{ formatBytes(item.spaceUsed) }}
+      </v-list-item-subtitle>
     </v-list-item-content>
   </v-list-item>
 </template>
@@ -13,6 +16,18 @@
 export default {
   props: {
     item: { type: Object, default: () => {} }
+  },
+  methods: {
+    formatBytes (a, b) {
+      if (a === 0) {
+        return '0 Bytes'
+      }
+      const c = 1024
+      const d = b || 2
+      const e = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+      const f = Math.floor(Math.log(a) / Math.log(c))
+      return `${parseFloat((a / Math.pow(c, f)).toFixed(d))} ${e[f]}`
+    }
   }
 }
 </script>
